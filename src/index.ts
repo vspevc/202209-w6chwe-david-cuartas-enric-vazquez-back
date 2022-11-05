ad8e168fc43f61f14d26146af08b170498f36b8f
David Cuartas <davidcuba91@gmail.com>
2022-11-05 12:20:21 +0100

    Add morgan and handle not found

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,17 @@ import "./loadEnvionment.js";
 import express from "express";
 import debugBuilder from "debug";
 import chalk from "chalk";
+import morgan from "morgan";
 
 const debug = debugBuilder("robots:server");
 const app = express();
 const port = process.env.PORT;
+
+app.use(morgan("dev"));
+
+app.use((req, res) => {
+  res.status(404).json({ error: "endpoint not found" });
+});
 
 app.listen(port, () => {
   debug(`server starter at: ${chalk.blueBright(`localhost:${port}`)}`);
