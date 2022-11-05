ac3171975b305b365c7aae154d72ea6eed150f2d
David Cuartas <davidcuba91@gmail.com>
2022-11-05 12:10:34 +0100

    Add debug and chalk

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import "./loadEnvionment.js";
 import express from "express";
+import debugBuilder from "debug";
+import chalk from "chalk";
 
+const debug = debugBuilder("robots:server");
 const app = express();
 const port = process.env.PORT;
 
 app.listen(port, () => {
-  console.log(`server starter at: localhost:${port}`);
+  debug(`server starter at: ${chalk.blueBright(`localhost:${port}`)}`);
 });
