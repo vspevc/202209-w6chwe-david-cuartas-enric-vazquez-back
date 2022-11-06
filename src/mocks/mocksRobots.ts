35bbc4231e4149b3bc8386cd6dd6adb23d423c27
David Cuartas <davidcuba91@gmail.com>
2022-11-06 11:37:02 +0100

    Add test robotcontrolles and mock objet

---
+++ src/mocks/mocksRobots.ts
@@ -0,0 +1,13 @@
+const mocksRobots = [
+  {
+    name: "",
+    imageUrl: "",
+    stats: {
+      speed: 2,
+      endurance: 2,
+      createAt: 2,
+    },
+  },
+];
+
+export default mocksRobots;
---
+++ src/server/controllers/robotsControllers.test.ts
@@ -0,0 +1,31 @@
+import type { Response } from "express";
+import Robot from "../../database/models/Robot";
+import mocksRobots from "../../mocks/mocksRobots.js";
+import getRobots from "./robotsControllers.js";
+
+const res: Partial<Response> = {
+  status: jest.fn().mockReturnThis(),
+  json: jest.fn(),
+};
+
+describe("Given a getRobots controllers", () => {
+  describe("When it receives a response return list of robots", () => {
+    test("Then it should  call its method status with 200", async () => {
+      const expectedStatus = 200;
+
+      Robot.find = jest.fn();
+
+      await getRobots(null, res as Response);
+
+      expect(res.status).toHaveBeenCalledWith(expectedStatus);
+    });
+
+    test("Then it should call its method json with", async () => {
+      Robot.find = jest.fn().mockReturnValue(mocksRobots);
+
+      await getRobots(null, res as Response);
+
+      expect(res.json).toHaveBeenCalledWith(mocksRobots);
+    });
+  });
+});
